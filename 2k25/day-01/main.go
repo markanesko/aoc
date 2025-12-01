fce864a66595f343754204434e639a4bec714156
markanesko <markoberic94@gmail.com>
2025-12-01 15:27:08 +0100

    2k25 day01

---
+++ 2k25/day-01/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+
+	filePath := os.Args[1]
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		return
+	}
+
+	fileScanner := bufio.NewScanner(file)
+	fileScanner.Split(bufio.ScanLines)
+
+	counter := 50
+	nullCounter := 0
+
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		if len(line) == 0 {
+			break
+		}
+		operation := line[0]
+		operand := line[1:]
+
+		operandValue, err := strconv.Atoi(operand)
+
+		if err != nil {
+			fmt.Printf("error formating operand value %s", operand)
+			break
+		}
+		numberOfCertainClicks := operandValue / 100
+
+		nullCounter += numberOfCertainClicks
+		operandValue = mod(operandValue, 100)
+
+		if string(operation) == "L" {
+			if operandValue > counter && counter != 0 {
+				nullCounter++
+			}
+			counter -= operandValue
+			counter = mod(counter, 100)
+		}
+
+		if string(operation) == "R" {
+			if counter+operandValue > 100 {
+				nullCounter++
+			}
+			counter += operandValue
+			counter = mod(counter, 100)
+		}
+
+		if counter == 0 {
+			nullCounter++
+		}
+	}
+
+	fmt.Printf("password is: %d\n", nullCounter)
+}
+
+func mod(a, b int) int {
+	return (a%b + b) % b
+}
